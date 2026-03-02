5f8613bda854afbe05db1b3e31c186ddeb886621
Abhishek Pandey <abhishek.p@Meditab.Local>
2026-03-02 18:52:41 +0530

    get_company_by_id function added to get company by id

--- app/crud/company.py
+++ app/crud/company.py
@@ -4,3 +4,6 @@ from app.models.company import Company  # ✅ correct model import
 
 def get_all_company(db: Session):
     return db.query(Company).all()
+
+def get_company_by_id(db: Session, company_id: int):
+    return db.query(Company).filter(Company.id == company_id).first()
--- app/routers/company.py
+++ app/routers/company.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter
 from app.crud import company as crud_company
 from app.schemas import company as schemas
 from app.database import get_db  # your DB session function under database
-from app.crud.company import get_all_company
+from app.crud.company import get_all_company, get_company_by_id
 
 router = APIRouter(
     prefix="/company",
@@ -19,3 +19,10 @@ def read_company(db: Session = Depends(get_db)):
     Read all company from the database
     """
     return crud_company.get_all_company(db)
+
+@router.post("/{company_id}", response_model=schemas.CompanyResponse)
+def get_company_id(company_id : int , db: Session = Depends(get_db)):
+    company = get_company_by_id(db,company_id)
+    if not company:
+        raise HTTPException(status_code=404, detail="Company not found")
+    return company
