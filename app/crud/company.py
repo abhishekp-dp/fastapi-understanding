3c8376386bde2fb16c68580bb0a4504c7c01f332
Abhishek Pandey <abhishek.p@Meditab.Local>
2026-03-03 19:17:41 +0530

    create company functionality added

--- app/crud/company.py
+++ app/crud/company.py
@@ -7,3 +7,19 @@ def get_all_company(db: Session):
 
 def get_company_by_id(db: Session, company_id: int):
     return db.query(Company).filter(Company.id == company_id).first()
+
+def create_company(db: Session, id: int, company_name: str, location: str):
+    # Step 1: Create object
+    new_company = Company(id=id, company_name=company_name,location=location)
+
+    # Step 2: Add to session
+    db.add(new_company)
+
+    # Step 3: Commit to database
+    db.commit()
+
+    # Step 4: Refresh (to get auto-generated ID)
+    db.refresh(new_company)
+
+    # Step 5: Return inserted user
+    return new_company
--- app/routers/company.py
+++ app/routers/company.py
@@ -6,7 +6,8 @@ from fastapi import APIRouter
 from app.crud import company as crud_company
 from app.schemas import company as schemas
 from app.database import get_db  # your DB session function under database
-from app.crud.company import get_all_company, get_company_by_id
+from app.crud.company import get_all_company, get_company_by_id, create_company
+from app.models import Company
 
 router = APIRouter(
     prefix="/company",
@@ -20,11 +21,23 @@ def read_company(db: Session = Depends(get_db)):
     """
     return crud_company.get_all_company(db)
 
-@router.post("/{company_id}", response_model=schemas.CompanyResponse)
+@router.get("/{company_id}", response_model=schemas.CompanyResponse)
 def get_company_id(company_id : int , db: Session = Depends(get_db)):
     company = get_company_by_id(db,company_id)
     if not company:
         raise HTTPException(status_code=404, detail="Company not found")
     return company
+
+
+@router.post("/createcompany/")
+def createcompany(id: int, company_name: str, location: str, db: Session = Depends(get_db)):
+    company= db.query(Company).filter(Company.id==id).first()
+    if company:
+        raise HTTPException(
+            status_code=400,
+            detail="Company already exist"
+        )
+    return create_company(db,id,company_name,location)
+
 
 
